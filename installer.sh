c67b73a8fc270c6c1b552987b2d25fabd17f06cd
0xmun1r <msmunirpappu@gmail.com>
2025-07-06 20:02:39 +0600

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "[*] Installing subranger ..."
+
+cp ./subranger.py /usr/local/bin/subranger
+chmod +x /usr/local/bin/subranger
+
+echo "[+] Installed! You can now run 'subranger' from anywhere."
